c43073859167ef298ad0a5d6e97225706ca31afc
timivarga <timea.varga252@gmail.com>
2022-12-08 10:01:32 +0100

    kesz

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 
 /**
@@ -8,7 +10,14 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+
+
+export const humans: HumanHero[] = [
+    {id: 3, name: "Thor", sex: "male", age: 23, health: 79,  },
+    {id: 4, name: "Bruce Wayne", sex: "male", age: 40, health: 100,  },
+    {id: 5, name: "Scarlet Witch", sex: "female", age: 31, health: 85,  },
+
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +26,16 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+
+/* const bumbleBee = new TransformerHero(2, "BumbleBee",4,4,"Transformers");
+const optimusPrime = new TransformerHero(1, "Optimus Prime",2, 4, "Transformers");
+const megatron = new TransformerHero(6,"Megatron", 0, 6, "Transformers");
+
+export const transformers: TransformerHero[] = [ bumbleBee, optimusPrime, megatron]; */
+
+ export const transformers: TransformerHero[] = [
+    {id: 2, name: "BumbleBee", wings: 4, wheels: 4, clan: "Transformers"},
+    {id: 1, name: "Optimus Prime", wings: 2, wheels: 4, clan: "Transformers"},
+    {id: 6, name: "Megatron", wings: 0, wheels: 6, clan: "Gonoszak"},
+
+];
--- src/transformer.ts
+++ src/transformer.ts
@@ -7,6 +7,37 @@ import { ITransformer } from './hero-interface';
  * Deklaráld benne az interfész által megkövetelt változókat.
  * Definiáld a konstruktort és kérj be minden változót, majd állítsd be őket.
  */
-export class TransformerHero {
+export class TransformerHero implements ITransformer {
+/*     id: number;
+    name:string;
+    wings: number;
+    wheels: number;
+    clan: string;
+    photo?: string;
+
+    constructor(azonosito:number, nev: string, szarnySzam:number, kerekSzam:number, klan: string, foto?: string){
+        this.id = azonosito;
+        this.name = nev;
+        this.wings = szarnySzam;
+        this.wheels = kerekSzam;
+        this.clan = klan;
+        this.photo = foto;
+
+    } */
+
+    constructor( public id: number,
+        public name:string,
+        public wings: number,
+        public wheels: number,
+        public clan: string,
+        public photo?: string){}
     
 }
+
+/* const urDongo = new TransformerHero(
+    1, 
+    "BumbleBee", 
+    0,
+    4,
+    "Transformers", 
+    "Camaro SS"); */
